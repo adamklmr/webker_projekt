87f973e4cd5d0df479998b6940abee863d5e2aa7
Kalmár Ádám <h363725@stud.u-szeged.hu>
2025-03-28 14:20:00 +0100

    t

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent,ProfileComponent,MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
