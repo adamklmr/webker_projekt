6f08309af9dde83cd96dae8ea88f8e44e86bc6ea
Kalmár Ádám <h363725@stud.u-szeged.hu>
2025-03-28 15:24:46 +0100

    haladgatok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { NgIf } from '@angular/common';
+import { ProductsComponent } from './pages/products/products.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent,ProfileComponent,MenuComponent],
+  imports: [RouterOutlet, HomeComponent,ProfileComponent,MenuComponent,ProductsComponent,AdminComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'horgaszbolt';
+
+  page = "home";
 }
+ 
