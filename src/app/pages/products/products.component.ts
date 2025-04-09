884824855f0fd63d6b7046d46697624f501690f6
Kalmár Ádám <h363725@stud.u-szeged.hu>
2025-04-09 23:39:49 +0200

    maxar

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule, MatCardTitleGroup } from '@angular/material/card';
 import { Product, ProductObject } from '../../shared/models/Product';
 import { MatButtonModule } from '@angular/material/button';
-
+import { FormsModule } from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOptionModule } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-products',
@@ -11,7 +16,12 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule, 
     MatCardModule,
-    MatButtonModule,            
+    MatButtonModule,      
+    FormsModule,
+    MatFormFieldModule,
+    MatLabel,
+    MatOptionModule,
+    MatInputModule
   ],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
@@ -20,11 +30,12 @@ import { MatButtonModule } from '@angular/material/button';
 export class ProductsComponent implements OnInit {
   ProductObject = ProductObject; // ProA termékek tömbje
   selectedIndex: number = 0; // Alapértelmezésben az első termék indexe
+  maxPrice: number = 0; // Alapértelmezett maximális ár
+  filteredProducts: Product[] = [...this.ProductObject];
 
   ngOnInit(): void {
 
   }
-
   reload(index: number): void {
     // Ellenőrizzük, hogy az index érvényes-e
     if (index >= 0 && index < this.ProductObject.length) {
@@ -34,5 +45,15 @@ export class ProductsComponent implements OnInit {
       console.error("Érvénytelen index:", index);
     }
   }
+  getFilteredProducts(): void {
+    console.log('Max Price:', this.maxPrice);
+    console.log('All Products:', this.ProductObject);
+    if (this.maxPrice !== null && !isNaN(this.maxPrice)) {
+      this.filteredProducts = this.ProductObject.filter(product => product.price <= this.maxPrice);
+    } else {
+      this.filteredProducts = [...this.ProductObject];
+    }
+  }
+
 }
 
